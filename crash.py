144d16ae02ce1868ef93679be5c1bb9a056a0386
Pauli Virtanen <pav@iki.fi>
2018-04-06 00:26:04 +0200

    reduce

--- crash.py
+++ crash.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 for j in range(200):
-    structure = np.recarray([1], dtype=[(('x', 'X'), np.object_)])
-    structure['x'][0] = np.array([2])
+    structure = np.array([1], dtype=[(('x', 'X'), np.object_)])
+    structure[0]['x'] = np.array([2])
